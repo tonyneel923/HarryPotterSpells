718c0ea622f2644d8ab85b0b2ea17cd981dc998f
tonyneel923 <tonyneel1127@gmail.com>
2013-11-11 00:35:41 -0500

    first

---
+++ src/com/example/harrypotterspells/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.harrypotterspells;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+//import com.phonegap.*;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
